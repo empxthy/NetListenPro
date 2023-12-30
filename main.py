7491a5ed93350393efd6eaa1e316bc49d4698a79
alexemployed <landshok35@gmail.com>
2023-12-30 16:54:35 +0400

    Added check update function

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import sys
 import os
 import asyncio
 import platform
+import requests
+import shutil
 import subprocess
 
 # Version
@@ -38,6 +40,44 @@ _lightBlue = "\033[1;34m"
 _lightPurple = "\033[1;35m"
 _lightCyan = "\033[1;36m"
 _lightWhite = "\033[1;37m"
+
+# Version
+_version = "1.0.0"
+
+# Check Update
+def check_update(repo_owner, repo_name, current_version):
+    print(f"{_yellow}[!]{_white}Checking for updates...")
+
+    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+
+        latest_release = response.json()
+        latest_version = latest_release["tag_name"]
+
+        if current_version >= latest_version:
+            print(f"{_green}[+]{_white}Your software is up to date (version {current_version}).")
+        else:
+            print(f"{_red}[-]{_white}A new version ({latest_version}) is available. Please update your software.")
+            upt = str(input(f"{_yellow}[!]{_white}Update now?: [{_green}y{_white}/{_red}n{_white}]\n{_yellow}[?]{_white}Y/N: "))
+            clone_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+            if upt == "y":
+                try:
+                    shutil.rmtree(clone_path)
+                    subprocess.run(["git", "clone", "https://github.com/alexemployed/NetListenPro.git", clone_path], check=True)
+                    print(f"{_green}[+]{_white}Repository cloned successfully!")
+                except subprocess.CalledProcessError as e:
+                    print(f"Error: {_red}{e}{_white}")
+            
+            if upt == "n":
+                print(f"{_red}[-]{_white}Update cancelled by user!")
+                sys.exit(1)
+    
+    except requests.exceptions.RequestException as e:
+        print(f"{_red}[-]{_white}Error: {e}")
+        print(f"Response content: {response.content}")
 
 
 # Privalages
